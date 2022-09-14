74a9f6d9afd5c32e8bb881a9db67b382f5cfae09
unknown <mirriaz5555@gmail.com>
2022-09-13 22:05:55 -0700

    Another project

---
+++ Employee/urls.py
@@ -0,0 +1,15 @@
+from . import views
+
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('remove_emp/<int:emp_id>', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
+    path('adminlogin', views.adminlogin, name='adminlogin'),
+    path('register', views.register, name='register'),
+
+]
